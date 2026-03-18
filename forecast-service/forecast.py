39eb784ca2829f0f0fbb12ff5da404d7d59db46b
Francesco Meloni <francesco.meloni6@studio.unibo.it>
2026-03-18 21:40:42 +0100

    forecast service is working

--- forecast-service/forecast.py
+++ forecast-service/forecast.py
@@ -1,10 +1,18 @@
 import time
 import requests
+import threading
 import pandas as pd
 from prophet import Prophet
 from datetime import datetime
+from flask import Flask
+from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
 
-PROM_URL = "http://prometheus.local"
+app = Flask(__name__)
+
+PRED_RANDOM = Gauge("predicted_random_rate", "Predicted random req/sec")
+PRED_HASH = Gauge("predicted_hash_rate", "Predicted hash req/sec")
+
+PROM_URL = "http://prometheus-server.default.svc:80"
 
 QUERY_RANDOM = "rate(random_requests_total[30m])"
 QUERY_HASH = "rate(hash_requests_total[30m])"
@@ -14,6 +22,10 @@ FORECAST_STEPS = 48
 
 history_random = []
 history_hash = []
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
 def query_prometheus(query):
 
@@ -41,6 +53,9 @@ def add_point(history, value):
 
 def train_model(history):
 
+    if len(history) < 10:
+        return None
+    
     df = pd.DataFrame(history)
 
     model = Prophet()
@@ -56,20 +71,22 @@ def forecast(model, history):
 
     future = model.make_future_dataframe(
         periods=FORECAST_STEPS,
-        freq="30m"
+        freq="5m"
     )
 
     forecast = model.predict(future)
 
     return forecast["yhat"].iloc[-1]
 
-def main():
+def loop():
 
     model_random = None
     model_hash = None
 
-    while True:
+    iterations = 0
 
+    while True:
+        print("iterations:", iterations)
         start_time = time.time()
 
         random_metric = query_prometheus(QUERY_RANDOM)
@@ -78,11 +95,17 @@ def main():
         add_point(history_random, random_metric)
         add_point(history_hash, hash_metric)
 
-        model_random = train_model(history_random)
-        model_hash = train_model(history_hash)
+        if iterations % 5 == 0:
+            model_random = train_model(history_random)
+            model_hash = train_model(history_hash)
+        
+        iterations += 1
 
         pred_random = forecast(model_random, history_random)
         pred_hash = forecast(model_hash, history_hash)
+
+        PRED_RANDOM.set(pred_random)
+        PRED_HASH.set(pred_hash)
 
         print(
             "random metric:", random_metric,
@@ -94,12 +117,16 @@ def main():
             "pred:", pred_hash,
         )
 
-        delta_time = start_time - time.time()
+        delta_time = time.time() - start_time
 
-        if delta_time > 0 and delta_time < 1800:
-            time.sleep(1800 - delta_time)
-        else:
-            time.sleep(1800)
+        if delta_time < 0 :
+            print("delta time < 0")
+            time.sleep(300)
+        elif delta_time < 300:
+            print("delta time < 300 delta_time:", delta_time)
+            time.sleep(300 - delta_time)
+
 
 if __name__ == "__main__":
-    main()
+    threading.Thread(target=loop).start()
+    app.run(host="0.0.0.0", port=8000)
